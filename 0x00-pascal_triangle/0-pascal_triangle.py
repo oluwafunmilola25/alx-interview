ab12b22e2d642625c3d150afd6373751b5f1ae58
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2024-10-02 13:00:23 -0500

    added changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,5 @@
 def pascal_triangle(n):
+
     if n <= 0:
         return []
     
@@ -7,11 +8,12 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
 
-        for j in range|(1, i):
+        for j in range|len(triangle[1 - i] - 1):
+            temp = triangle[i - 1]
 
-            row.append(triangle[i-1] [j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
             row.append(1)
 
             triangle.append(row)
 
-            return triangle
+        return triangle
