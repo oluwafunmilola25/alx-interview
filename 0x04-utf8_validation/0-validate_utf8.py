91bf36388d77706a0fd79d8bbd2d558045338f75
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2024-11-04 02:58:18 -0500

    updated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,25 +8,28 @@ def get_leading_set_bits(num):
     helper = 1 << 7
     while helper & num:
         set_bits += 1
-        helper = helper >> 1
+        helper >>= 1
     return set_bits
 
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
     bits_count = 0
-    for i in range(len(data)):
+    for byte in data:
         if bits_count == 0:
-            bits_count = get_leading_set_bits(data[i])
+            bits_count = get_leading_set_bits(byte)
             '''1-byte (format: 0xxxxxxx)'''
             if bits_count == 0:
                 continue
             '''a character in UTF-8 can be 1 to 4 bytes long'''
             if bits_count == 1 or bits_count > 4:
                 return False
-            else:
-                '''checks if current byte has format 10xxxxxx'''
-                if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
-                    return False
-                bits_count -= 1
-                return bits_count == 0
+            '''decrement to handle the remaining continuation bytes'''
+            bits_count -= 1
+        else:
+            '''checks if current byte has format 10xxxxxx'''
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
+                return False
+            bits_count -= 1
+
+            return bits_count == 0
